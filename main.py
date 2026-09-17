4d59dd06b2f51f86a2617a19296dfea1752c5ceb
Vignesh-CP <vigneshcp1511@gmail.com>
2026-09-18 00:10:04 +0530

    Initial commit with CodeBuild files

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from mangum import Mangum
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the Equipment Telemetry API"}
+
+@app.get("/test")
+def read_test():
+    return {"message": "The test route is working perfectly"}
+
+# Mangum adapter for AWS Lambda
+handler = Mangum(app)
